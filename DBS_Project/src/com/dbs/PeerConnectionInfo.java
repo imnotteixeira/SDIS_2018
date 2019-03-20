2036861851d679d042a8e5f4312817af0bddf3b2
Henrique Lima <henriquelim@hotmail.com>
2019-03-20 11:03:29 +0000

    Further advances in storer

--- DBS_Project/src/com/dbs/PeerConnectionInfo.java
+++ DBS_Project/src/com/dbs/PeerConnectionInfo.java
@@ -4,14 +4,19 @@ import java.net.MulticastSocket;
 
 public class PeerConnectionInfo {
 
-    private int senderId;
+    private int senderId, controlPort, backupPort, recoveryPort;
     private byte[] version;
+    String multicastHostname;
 
     private MulticastSocket controlChanelSocket, backupChannelSocket, recoveryChannelSocket;
 
-    public PeerConnectionInfo(int senderId, byte[] version, MulticastSocket controlChanelSocket, MulticastSocket backupChannelSocket, MulticastSocket recoveryChannelSocket) {
+    public PeerConnectionInfo(int senderId, byte[] version, String multicastHostname, int controlPort, int backupPort, int recoveryPort, MulticastSocket controlChanelSocket, MulticastSocket backupChannelSocket, MulticastSocket recoveryChannelSocket) {
         this.senderId = senderId;
+        this.controlPort = controlPort;
+        this.backupPort = backupPort;
+        this.recoveryPort = recoveryPort;
         this.version = version;
+        this.multicastHostname = multicastHostname;
         this.controlChanelSocket = controlChanelSocket;
         this.backupChannelSocket = backupChannelSocket;
         this.recoveryChannelSocket = recoveryChannelSocket;
@@ -21,8 +26,24 @@ public class PeerConnectionInfo {
         return senderId;
     }
 
+    public int getControlPort() {
+        return controlPort;
+    }
+
+    public int getBackupPort() {
+        return backupPort;
+    }
+
+    public int getRecoveryPort() {
+        return recoveryPort;
+    }
+
     public byte[] getVersion() {
         return version;
+    }
+
+    public String getMulticastHostname() {
+        return multicastHostname;
     }
 
     public MulticastSocket getControlChanelSocket() {
--- DBS_Project/src/com/dbs/Storer.java
+++ DBS_Project/src/com/dbs/Storer.java
@@ -2,6 +2,11 @@ package com.dbs;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,12 +22,12 @@ public class Storer {
 
     public void run(){
         while(true){
-            PUTCHUNKListener();
+            PutchunkMessage PCmessage = PUTCHUNKListener();
+            sendResponse(PCmessage);
         }
     }
 
-
-    private DatagramPacket PUTCHUNKListener(){
+    private PutchunkMessage PUTCHUNKListener(){
         byte[] buf = new byte[Storer.BUF_SIZE];
         DatagramPacket packet = new DatagramPacket(buf, buf.length);
 
@@ -32,17 +37,40 @@ public class Storer {
             e.printStackTrace();
         }
 
-        decodePUTCHUNK(new String(packet.getData(), 0, packet.getLength()));
-
-        return packet;
+        return decodePUTCHUNK(new String(packet.getData(), 0, packet.getLength()));
     }
 
-    private void decodePUTCHUNK(String encodedData){
+    private void sendResponse(PutchunkMessage PCmessage) {
+        int random = (int) Math.random()*400;
+
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+        executor.schedule(()-> sendSTORED(PCmessage), random, TimeUnit.MILLISECONDS);
+
+        executor.shutdown();
+    }
+
+    TROCAR PARA USAR PUTCHUNKMESSAGE.FROMSTRING
+    private PutchunkMessage decodePUTCHUNK(String encodedData){
         Pattern r = Pattern.compile("(PUTCHUNK)\\s(\\d.\\d)\\s(\\d+)\\s(\\w+)\\s(\\d{1,6})\\s(\\d)");
 
         Matcher m = r.matcher(encodedData);
 
 
+    }
+
+    private void sendSTORED(PutchunkMessage receivedPUTCHUNK){
+        byte[] buf = "STORED <Version> <SenderId> <FileId> <ChunkNo> <CRLF><CRLF>".getBytes();
+
+        try {
+            DatagramPacket packet = new DatagramPacket(buf, buf.length,
+                InetAddress.getByName(connectionInfo.getMulticastHostname()), connectionInfo.getControlPort());
+                connectionInfo.getControlChanelSocket().send(packet);
+        }catch(UnknownHostException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
